820f8be1e6744bc740402287f90224f890db66f1
Rajan Chaudhary <rajann44@gmail.com>
2023-07-24 23:11:09 +0200

    Home and Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Home></Home>
+    </>
+  );
 }
 
 export default App;
